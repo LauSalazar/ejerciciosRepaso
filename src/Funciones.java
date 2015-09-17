e34885f6398a68d68fb8113dbb8b03404e150730
LauSalazar <laurasalazarh@gmail.com>
2015-09-17 18:27:06 -0500

    Correccion de la funcion sumaDiagonales, para que sume las diagonales de cualquier matriz cuadrada

--- src/Funciones.java
+++ src/Funciones.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 /**
  * Ejercicios de repaso de lógica de programación
+ * 
  * @author lSalazar
  *
  */
 public class Funciones {
 	/**
-	 * Cuenta cuantas veces esta cada letra en una palabra  
-	 * @param A Palabra 
+	 * Cuenta cuantas veces esta cada letra en una palabra
+	 * 
+	 * @param A
+	 *            Palabra
 	 * @return Arreglo de 26 posiciones con las cuenta de cada letra
 	 */
 	public int[] getCountAnagram(char[] A) {
@@ -18,10 +21,12 @@ public class Funciones {
 		}
 		return abcA;
 	}
-	
+
 	/**
 	 * Devuelve el valor absoluto de un número entero
-	 * @param a Numero
+	 * 
+	 * @param a
+	 *            Numero
 	 * @return Valor absoluto de a
 	 */
 	public int absoluto(int a) {
@@ -30,11 +35,14 @@ public class Funciones {
 		else
 			return -a;
 	}
-	
+
 	/**
 	 * Realiza la división entre dos enteros
-	 * @param a Dividendo
-	 * @param b Divisor
+	 * 
+	 * @param a
+	 *            Dividendo
+	 * @param b
+	 *            Divisor
 	 * @return ResultadoDividendo con el resultado de la operación
 	 */
 	public ResultadoDivision division(int a, int b) {
@@ -64,11 +72,14 @@ public class Funciones {
 		RD.setResiduo(aux);
 		return RD;
 	}
-	
+
 	/**
 	 * Búsqueda binaria de un valor en un arreglo
-	 * @param arreglo Datos en los que se buscará el valor
-	 * @param valor Dato a encontrar
+	 * 
+	 * @param arreglo
+	 *            Datos en los que se buscará el valor
+	 * @param valor
+	 *            Dato a encontrar
 	 * @return Posición del valor en el arreglo o -1 si no lo encuentra
 	 */
 	public int busquedaB(int[] arreglo, int valor) {
@@ -86,13 +97,18 @@ public class Funciones {
 		}
 		return -1;
 	}
-	
+
 	/**
 	 * Búsqueda binaria recursiva de un valor en un arreglo
-	 * @param arreglo Datos en los que se buscará el valor
-	 * @param valor Dato a encontrar
-	 * @param p primera posición
-	 * @param u última posición
+	 * 
+	 * @param arreglo
+	 *            Datos en los que se buscará el valor
+	 * @param valor
+	 *            Dato a encontrar
+	 * @param p
+	 *            primera posición
+	 * @param u
+	 *            última posición
 	 * @return Posición del valor en el arreglo o -1 si no lo encuentra
 	 */
 	public int busquedaBR(int[] arreglo, int valor, int p, int u) {
@@ -107,10 +123,12 @@ public class Funciones {
 			pos = -1;
 		return pos;
 	}
-	
+
 	/**
 	 * Algoritmo babilónico para el cálculo de la raiz cuadrada
-	 * @param x Radicando
+	 * 
+	 * @param x
+	 *            Radicando
 	 * @return Raiz cuadrada de x
 	 */
 	public double raizCuadrada(double x) {
@@ -124,9 +142,13 @@ public class Funciones {
 	}
 
 	/**
-	 * Compara dos listas ligadas y devuelve los valores que son compartidos entre estas dos
-	 * @param cabeza1 Nodo que apunta a la cabeza de la primera lista
-	 * @param cabeza2 Nodo que apunta a la cabeza de la segunda lista
+	 * Compara dos listas ligadas y devuelve los valores que son compartidos
+	 * entre estas dos
+	 * 
+	 * @param cabeza1
+	 *            Nodo que apunta a la cabeza de la primera lista
+	 * @param cabeza2
+	 *            Nodo que apunta a la cabeza de la segunda lista
 	 * @return Lista con los valores repetidos
 	 */
 	public ListaLigada repetidos(Nodo cabeza1, Nodo cabeza2) {
@@ -152,11 +174,14 @@ public class Funciones {
 		}
 		return listaNueva;
 	}
-	
+
 	/**
-	 * Cálculo de raiz cuadrada con precisión 
-	 * @param x Radicando
-	 * @param precision Indica que el resultado puede estar +/- la precisión 
+	 * Cálculo de raiz cuadrada con precisión
+	 * 
+	 * @param x
+	 *            Radicando
+	 * @param precision
+	 *            Indica que el resultado puede estar +/- la precisión
 	 * @return Raiz cuadrada de x +/- la precision
 	 * @throws Exception
 	 */
@@ -178,10 +203,10 @@ public class Funciones {
 		return medio;
 	}
 
-
 	/**
 	 * Devuelve si existen dos numeros en el arreglo que sumados den x
-	 * @param arreglo 
+	 * 
+	 * @param arreglo
 	 * @param x
 	 * @return Booleano
 	 */
@@ -198,14 +223,17 @@ public class Funciones {
 		}
 		return false;
 	}
-	
 
 	/**
-	 * Encuentra los valores repetidos de dos listas ligadas y los guarda en una tercera
-	 * Este método usa un HashSet como ayuda
-	 * @param cabeza1 Nodo que apunta al primer elemento de una de las listas
-	 * @param cabeza2 Nodo que apunta al primer elemento de la otra listas
-	 * @return Nodo que apunta al primer elemento de la lista con los elementos repetidos en ambas listas
+	 * Encuentra los valores repetidos de dos listas ligadas y los guarda en una
+	 * tercera Este método usa un HashSet como ayuda
+	 * 
+	 * @param cabeza1
+	 *            Nodo que apunta al primer elemento de una de las listas
+	 * @param cabeza2
+	 *            Nodo que apunta al primer elemento de la otra listas
+	 * @return Nodo que apunta al primer elemento de la lista con los elementos
+	 *         repetidos en ambas listas
 	 */
 	public static Nodo repetidos2(Nodo cabeza1, Nodo cabeza2) {
 		if (cabeza1 == null || cabeza2 == null)
@@ -228,38 +256,51 @@ public class Funciones {
 		nodoApuntador.setSiguiente(listaNueva.getPrimero());
 		return nodoApuntador;
 	}
-	
+
 	/**
 	 * Devuelve una matriz nxn con los elementos de un triangulo de pascal
-	 * @param n Define el tamaño de la matriz cuadrada
+	 * 
+	 * @param n
+	 *            Define el tamaño de la matriz cuadrada
 	 * @return Matriz con elementos de un triangulo de pascal
 	 */
-	public static int[][] matrizPascal(int n){
+	public static int[][] matrizPascal(int n) {
 		int[][] matriz = new int[n][n];
-		for(int i=0; i<n;i++){
-			for(int j=0;j<n;j++){
-				if(i==0 && j==0) matriz[i][j]=1;
-				else if(j==0) matriz[i][j]=1;
-				else if(i>0 && j<=i){
-					matriz[i][j]=matriz[i-1][j-1]+matriz[i-1][j];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i == 0 && j == 0)
+					matriz[i][j] = 1;
+				else if (j == 0)
+					matriz[i][j] = 1;
+				else if (i > 0 && j <= i) {
+					matriz[i][j] = matriz[i - 1][j - 1] + matriz[i - 1][j];
 				}
 			}
-		}		
-		return matriz;		
+		}
+		return matriz;
 	}
-	
+
 	/**
-	 * Suma los datos de las diagonales principal y secundaria de una matriz
+	 * Suma los datos de las diagonales principal y secundaria de una matriz cuadrada
 	 * @param matriz
-	 * @param n
+	 * @param n Tamaño de la matriz (nxn)
 	 * @return Suma de los datos
 	 */
 	public static int sumaDiagonales(int[][] matriz, int n){ //Lo puse estatico porque no quería declarar un objeto de tipo Funciones...
 		int suma=0;
+		int medio = n/2;
 		for(int i =0; i<n; i++){
-			suma= suma + matriz[i][i]+matriz[i][n-1-i];
-		}
+			if(n%2==0){
+				suma= suma + matriz[i][i]+matriz[i][n-1-i];
+			}
+			else if(i==medio){
+				suma= suma + matriz[i][i];
+				}
+			else{
+				suma= suma + matriz[i][i]+matriz[i][n-1-i];
+				}
+			}
+			
 		return suma;
-	}	
-
+	}
 }
